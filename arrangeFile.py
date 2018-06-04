6ce2b6fb3ddaf1ea8f357257a9e8124e915fcef2
YichenLi666 <yl3506@nyu.edu>
2018-06-03 23:50:42 -0400

    sessions and func rearrange

--- arrangeFile.py
+++ arrangeFile.py
@@ -1,40 +1,62 @@
 # this script is to rearrange data files in 
 # forrest dataset to allign with BIDS format
-import os, json
+import os, json, fnmatch
 
 # change to main directory
-main_directory = '/mindhive/saxelab3/anzellotti/forrest/forrest_bids/'
+main_directory = '/mindhive/saxelab3/anzellotti/forrest/forrest_bids'
 os.chdir(main_directory)
 
 # write dataset description in json
 data = {}
-data['BIDSVersion'] = []
-data['BIDSVersion'].append("1.0.0")
-data['License'] = []
-data['License'].append("https://www.nature.com/articles/sdata201692#abstract")
-data['Name'] = []
-data['Name'].append("A studyforrest extension, simultaneous fMRI and eye gaze recordings during prolonged natural stimulation")
-data['ReferencesAndLinks'] = []
-data['ReferencesAndLinks'].append({"http://dx.doi.org/10.1038/sdata.2016.92"})
-
+data['Name'] = "A studyforrest extension, simultaneous fMRI and eye gaze recordings during prolonged natural stimulation,"
+data['License'] = "https://www.nature.com/articles/sdata201692#abstract,"
+data['BIDSVersion'] = "1.0.0"
+data['ReferencesAndLinks'] = "http://dx.doi.org/10.1038/sdata.2016.92"
+with open('dataset_description.json', 'w') as out_file:
+	json.dump(data, out_file)
 
 # get all subject folders in main directory
-all_sub_folder = os.listdir(main_directory)
+all_folder_names = os.listdir(main_directory)
 # iterate through all subjects to arrange files
-for sub_folder in all_sub_folder:
+for name in all_folder_names:
+	if name == '.DS_Store' or name == 'dataset_description.json':
+		continue
 	# enter current subject folder
+	os.chdir(main_directory+'/'+name)
 	# create new folders ses-localizer and ses-movie
+	if not os.path.exists('ses-localizer'):
+		os.mkdir('ses-localizer')
+	if not os.path.exists('ses-movie'):
+		os.mkdir('ses-movie')
 	# create new subfolder ses-localizer/func, anat (TBD)
-	# and ses-movie/func, anat (TBD)
+	os.chdir('ses-localizer')
+	if not os.path.exists('func'):
+		os.mkdir('func')
+	# create new subfolder ses-movie/func, anat (TBD)
+	os.chdir(main_directory+'/'+name+'/ses-movie')
+	if not os.path.exists('func'):
+		os.mkdir('func')
 	# enter sub/func folder
+	os.chdir(main_directory+'/'+name+'/func')
 	# iterate through all files in func 
-	# move correspond files to ses-loc/func
-	# move correspond files to ses-movie/func
+	all_func = os.listdir('.')
+	for func in all_func:
+		# move correspond files to ses-loc/func
+		if fnmatch.fnmatch(func, '*ses-localizer*.*.*') or fnmatch.fnmatch(func, '*ses-localizer*.*'):
+			os.rename(func, main_directory+'/'+name+'/ses-localizer/func/'+func)
+		# move correspond files to ses-movie/func
+		if fnmatch.fnmatch(func, '*ses-movie*.*.*') or fnmatch.fnmatch(func, '*ses-movie*.*'):
+			os.rename(func, main_directory+'/'+name+'/ses-movie/func/'+func)
 
 	# cd to previous level anat folder
-	# check if exists T1w_defacemask file
-	# if so, change name to T12-defacemask
-	# change name to ses-loc/movie
+	os.chdir(main_directory+'/'+name+'/anat')	
+	for anat in os.listdir('.'):
+		# check if exists T1w_defacemask file
+		if fnmatch.fnmatch(anat, '*T1w_defacemask.*'):
+			# if so, change name to T12-defacemask
+			anat_name = name+'_T1w-defacemask.nii.gz'
+			os.rename(anat, main_directory+'/'+name+'/anat/'+anat_name)
+	# change name to ses-loc/movie (TBD)
 	
 	# cd to main directory
 	# copy sub/anat to sub/ses-movie
