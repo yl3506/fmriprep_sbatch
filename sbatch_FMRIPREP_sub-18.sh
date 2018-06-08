50ac8c5afad0b90f7eaeac1229ea4c64c2c2afc7
YichenLi666 <yl3506@nyu.edu>
2018-06-08 15:31:14 -0400

    erase email

--- sbatch_FMRIPREP_sub-18.sh
+++ sbatch_FMRIPREP_sub-18.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1 --cpus-per-task=16  --tasks-per-node=2
 #SBATCH --mem=50GB
 #SBATCH --time=5-00:00:00
-#SBATCH --mail-user=yichenli@mit.edu --mail-type=ALL
+#SBATCH --mail-user=xxx@mit.edu --mail-type=ALL
 #SBATCH --output=logs/sbatchlog_FMRIPREP_stdout_%j.txt
 #SBATCH --error=logs/sbatchlog_FMRIPREP_stderr_%j.txt
 #SBATCH --qos=saxelab
