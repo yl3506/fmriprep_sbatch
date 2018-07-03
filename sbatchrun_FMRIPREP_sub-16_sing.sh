d84ea7b48692687b8788cc6d320a1e51b8781860
yl3506 <yl3506@nyu.edu>
2018-07-03 17:15:23 -0400

    tutorial notes for fmriprep

--- sbatchrun_FMRIPREP_sub-16_sing.sh
+++ sbatchrun_FMRIPREP_sub-16_sing.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name=fmriprep
-#SBATCH --nodes=1 --cpus-per-task=16  --tasks-per-node=1
-#SBATCH --mem=200GB
-#SBATCH --time=5-00:00:00
-#SBATCH --mail-user=yichenli@mit.edu --mail-type=ALL
-#SBATCH --output=logs/sbatchlog_FMRIPREP_stdout_%j.txt
-#SBATCH --error=logs/sbatchlog_FMRIPREP_stderr_%j.txt
-#SBATCH --qos=saxelab
-chmod +x sbatchrun_FMRIPREP_sub-16_sing.sh
-srun ./sbatchrun_FMRIPREP_sub-16_sing.sh
--bash-4.2$ cat sbatchrun_FMRIPREP_sub-16_sing.sh
-#!/bin/bash
 exp_path="/mindhive/saxelab3/anzellotti/forrest"
 DOW="Wed"
 cd "/mindhive/saxelab3/anzellotti/software/preproc_fmriprep"
